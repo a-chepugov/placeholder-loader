48fd6ae3a38fd0964e068292456ea45a977693d6
a.chepugov <oleksii.chepugov@gmail.com>
2017-10-25 15:59:17 +0300

    chore

--- index.js
+++ index.js
@@ -10,32 +10,34 @@ function isString(string) {
 }
 
 module.exports = function (source) {
-	let {handler, placeholder} = loaderUtils.getOptions(this) || {};
-	if (isString(source) && isString(placeholder)) {
-		// Пытаемся найти placeholder
-		let index = source.indexOf(placeholder);
-		if (index !== -1) {
-			// Если обработчик указан как строка, пытаемся найти в проекте соответствующий файл
-			if (isString(handler)) {
-				let handlerPath = path.resolve(handler);
-				try {
-					handler = require(handlerPath);
-				} catch (error) {
-					error.message += `${packageName} => handler is not found in ${handlerPath}`;
-					throw error;
+	if (isString(source)) {
+		let {handler, placeholder} = loaderUtils.getOptions(this) || {};
+		if (isString(placeholder)) {
+			// Пытаемся найти placeholder
+			let index = source.indexOf(placeholder);
+			if (index !== -1) {
+				// Если обработчик указан как строка, пытаемся найти в проекте соответствующий файл
+				if (isString(handler)) {
+					let handlerPath = path.resolve(handler);
+					try {
+						handler = require(handlerPath);
+					} catch (error) {
+						error.message += `${packageName} => handler is not found in ${handlerPath}`;
+						throw error;
+					}
 				}
-			}
-			let replacement;
-			if (handler instanceof Function) {
-				// Получаем замену для placeholder'а
-				replacement = handler.apply(this, arguments)
-			} else {
-				throw new Error(`${packageName} => handler in not a function`);
-			}
 
-			if (isString(replacement)) {
-				// Подменяем placeholder на replacement
-				return source.replace(placeholder, replacement)
+				let replacement;
+				if (handler instanceof Function) {
+					// Получаем замену для placeholder'а
+					replacement = handler.apply(this, arguments)
+				} else {
+					throw new Error(`${packageName} => handler in not a function`);
+				}
+				if (isString(replacement)) {
+					// Подменяем placeholder на replacement
+					return source.replace(placeholder, replacement)
+				}
 			}
 		}
 	}
