6492765b1b40ca2c6533cbeb4210ca57a6b44bca
Алексей <oleksii.chepugov@ria.com>
2018-02-02 12:22:50 +0200

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp');
+
+const package = require('./package.json');
+const {name, version} = package;
+
+const paths = {
+	source: 'index.js',
+	doc: 'doc',
+};
+
+gulp.task('clean', () => {
+	const del = require('del');
+	return del([paths.doc]);
+});
+
+gulp.task('documentation-md', function () {
+	const gulpDocumentation = require('gulp-documentation');
+	return gulp.src(paths.source)
+		.pipe(gulpDocumentation('md', {}, {name, version}))
+		.pipe(gulp.dest(paths.doc));
+});
+
+gulp.task('documentation-html', function () {
+	const gulpDocumentation = require('gulp-documentation');
+	return gulp.src(paths.source)
+		.pipe(gulpDocumentation('html', {}, {name, version}))
+		.pipe(gulp.dest(paths.doc));
+});
+
+gulp.task("watch:docs", () => {
+	const watcher = gulp.watch(paths.source, gulp.series('documentation-html'));
+	watcher.on('change', function (path, stats) {
+		console.log('File ' + path + ' was changed');
+	});
+	return watcher;
+});
